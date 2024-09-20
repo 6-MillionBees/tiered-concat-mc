0a5fcf124e4fe07934e8c17f80e31cf4def09c13
6MillionBees <BoettcherAr321@northwested.org>
2024-09-20 10:36:48 -0400

    Update concat_mc.py

--- concat_mc.py
+++ concat_mc.py
@@ -1,7 +1,6 @@
 # Arden Boettcher
 # 9/20/24
 # Concat MC
-from colorama import Back
 
 # Task 1:
 
@@ -14,4 +13,10 @@ print('\nHi my name is ' + my_name + ', and my favorite game is ' + fav_game + '
 
 print('What I\'ve learned about concatenation: ')
 print('Concatnaiton doesn\'t automatically add spaces, ' + 'you have to do that yourself.')
-print('F-strings are better than concatnation in the context of printing variables in strings, but concatnaiton is still a very useful thing to know about.\n')
+print('You can\'t do concatnation with numeric data, you have to make it a string using the str() func first.\n')
+
+# Task 3: 
+
+chicken_fries = 4.79
+
+print('Chicken fries cost $' + str(chicken_fries) +'.\n')
