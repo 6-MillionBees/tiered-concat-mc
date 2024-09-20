d4895122362f7cceadeb5f4467c0362039cef4df
6MillionBees <BoettcherAr321@northwested.org>
2024-09-20 10:30:07 -0400

    Add files via upload

---
+++ concat_mc.py
@@ -0,0 +1,17 @@
+# Arden Boettcher
+# 9/20/24
+# Concat MC
+from colorama import Back
+
+# Task 1:
+
+my_name = 'Arden Boettcher'
+fav_game = 'Team Fortress 2'
+
+print('\nHi my name is ' + my_name + ', and my favorite game is ' + fav_game + '.\n')
+
+# Task 2:
+
+print('What I\'ve learned about concatenation: ')
+print('Concatnaiton doesn\'t automatically add spaces, ' + 'you have to do that yourself.')
+print('F-strings are better than concatnation in the context of printing variables in strings, but concatnaiton is still a very useful thing to know about.\n')
